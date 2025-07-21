b27c1886aae71ea4b439523f0856739fdbddec9b
tikrack <tikrackcode@gmail.com>
2025-07-21 17:30:23 +0330

    [Icon] -> add icon sax packages

--- src/components/Authentication/index.jsx
+++ src/components/Authentication/index.jsx
@@ -1,9 +1,48 @@
-import {Button} from "@heroui/button";
+import {Button, Form, Input} from "@heroui/react";
+import {useState} from "react";
+import {useTranslation} from "react-i18next";
 
 const Authentication = () => {
+    const [action, setAction] = useState(null);
+    const {t} = useTranslation()
+
     return (<>
         <main className={"h-screen flex justify-center items-center"}>
+            <div className="border border-gray-300 p-4 min-h-[350px] w-full max-w-[450px] rounded-[20px]">
+                <h1 className={"text-[30px] font-bold"}>ورود</h1>
+                <Form className="size-full">
+                    <Input
+                        isRequired
+                        label={t("userName")}
+                        errorMessage="لطفا یک مقدار وارد کنید"
+                        labelPlacement="outside"
+                        name="username"
+                        placeholder="نام کاربری را وارد کنید"
+                        type="text"
+                    />
 
+                    <Input
+                        isRequired
+                        errorMessage="Please enter a valid email"
+                        label="Email"
+                        labelPlacement="outside"
+                        name="email"
+                        placeholder="Enter your email"
+                        type="email"
+                    />
+                    <div className="flex gap-2">
+                        <Button color="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Button type="reset" variant="flat">
+                            Reset
+                        </Button>
+                    </div>
+                    {action && (<div className="text-small text-default-500">
+                        Action: <code>{action}</code>
+                    </div>)}
+                </Form>
+            </div>
         </main>
     </>)
 }
--- src/components/Routes/Provider.jsx
+++ src/components/Routes/Provider.jsx
@@ -1,17 +1,22 @@
 import {HeroUIProvider} from "@heroui/system";
-import {useHref, useNavigate} from "react-router";
-import {Outlet} from "react-router";
+import {Outlet, useHref, useNavigate} from "react-router";
+import {I18nextProvider, useTranslation} from "react-i18next";
+import i18n from "@components/Services/Translations/i18n.js";
 
 const Provider = () => {
     const navigate = useNavigate();
+    const {i18n: translate} = useTranslation();
+    translate.changeLanguage("fa-IR");
 
-    return (
+    return (<>
         <HeroUIProvider navigate={navigate} useHref={useHref}>
-            <div className={"max-w-[1536px] h-full w-full mx-auto"}>
-                <Outlet />
-            </div>
+            <I18nextProvider i18n={i18n}>
+                <div className={"max-w-[1536px] h-full w-full mx-auto"}>
+                    <Outlet/>
+                </div>
+            </I18nextProvider>
         </HeroUIProvider>
-    );
+    </>);
 }
 
 export default Provider;
