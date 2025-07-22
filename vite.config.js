63e96e7d8313e1aae759cd3ee7961132d3eb2b68
tikrack <tikrackcode@gmail.com>
2025-07-22 16:56:52 +0330

    [Https] -> add https protocle

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), basicSsl()],
+    server: {
+        port: 3000,
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
